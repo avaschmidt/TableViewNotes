d9b550e2426e49ebf1db384e0c14da925fbd0c3c
Ava Schmidt <avamschmidt07@gmail.com>
2024-10-08 07:27:15 -0500

    Initial Commit

---
+++ TableViewNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewNotes
+//
+//  Created by AVA SCHMIDT on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
